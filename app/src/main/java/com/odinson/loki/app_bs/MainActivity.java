7d9ca60353ea9b11fc975ccc3bef0427eea6a59f
shadowwa1k3r <ergash1994@gmail.com>
2017-10-15 13:19:51 +0500

    1st time

---
+++ app/src/main/java/com/odinson/loki/app_bs/MainActivity.java
@@ -0,0 +1,13 @@
+package com.odinson.loki.app_bs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
